a3b69f089b2711f690ee58454a5383ca9d81ac7d
deepak-linux <deepak008@live.com>
2024-10-20 19:36:24 +0530

    Refactor base.html and siderbar.html templates

--- src/routes/api.py
+++ src/routes/api.py
@@ -7,14 +7,24 @@ from flask import jsonify, blueprints, request, render_template
 from flask_login import login_required, current_user
 from flask_compress import Compress
 from src.config import app, db, csrf
-from src.models import UserDashboardSettings, AlertTicket, ChartConfiguration, UserProfile
-from src.utils import _collect_metrics, get_os_release_info, get_os_info, get_cached_value
+from src.models import (
+    UserDashboardSettings,
+    AlertTicket,
+    ChartConfiguration,
+    UserProfile,
+)
+from src.utils import (
+    _collect_metrics,
+    get_os_release_info,
+    get_os_info,
+    get_cached_value,
+)
 from src.routes.helper.common_helper import admin_required
 from src.routes.helper.prometheus_helper import (
-    load_prometheus_config, 
-    save_prometheus_config, 
-    load_alert_rules, 
-    save_alert_rules
+    load_prometheus_config,
+    save_prometheus_config,
+    load_alert_rules,
+    save_alert_rules,
 )
 
 api_bp = blueprints.Blueprint("api", __name__)
@@ -23,8 +33,9 @@ cache = {}
 compress = Compress(app)
 
 PROMETHEUS_BASE_URL = "http://localhost:9090"
-QUERY_API_URL = f'{PROMETHEUS_BASE_URL}/api/v1/query_range'
-TARGETS_API_URL = f'{PROMETHEUS_BASE_URL}/api/v1/targets'
+QUERY_API_URL = f"{PROMETHEUS_BASE_URL}/api/v1/query_range"
+TARGETS_API_URL = f"{PROMETHEUS_BASE_URL}/api/v1/targets"
+
 
 @app.route("/api/v1/system-info", methods=["GET"])
 @csrf.exempt
@@ -34,36 +45,50 @@ def system_api():
         system_info = _collect_metrics()
         return jsonify(system_info), 200
     except Exception as e:
-        return jsonify({"error": "An error occurred while fetching the system information", "details": str(e)}), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while fetching the system information",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
+
 
 async def fetch_metric(session, metric_query, start_time, end_time, step):
-    params = {
-        'query': metric_query,
-        'start': start_time,
-        'end': end_time,
-        'step': step
-    }
+    params = {"query": metric_query, "start": start_time, "end": end_time, "step": step}
     async with session.get(QUERY_API_URL, params=params) as response:
         return await response.json()
 
+
 async def fetch_all_metrics(metrics, start_time, end_time, step):
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_metric(session, query, start_time, end_time, step) for query in metrics]
+        tasks = [
+            fetch_metric(session, query, start_time, end_time, step)
+            for query in metrics
+        ]
         results = await asyncio.gather(*tasks)
         return results
 
+
 def get_cached_data(cache_key):
     return cache.get(cache_key)
+
 
 def set_cached_data(cache_key, data, timeout=60):
     cache[cache_key] = data
 
 
-@app.route('/api/v1/prometheus/graphs_data', methods=['GET'])
+@app.route("/api/v1/prometheus/graphs_data", methods=["GET"])
 @login_required
 def graph_data_api():
     try:
-        PROMETHEUS_METRICS = ChartConfiguration.query.with_entities(ChartConfiguration.metric_name).filter_by(user_id=current_user.id).all()
+        PROMETHEUS_METRICS = (
+            ChartConfiguration.query.with_entities(ChartConfiguration.metric_name)
+            .filter_by(user_id=current_user.id, is_active=True)
+            .all()
+        )
         PROMETHEUS_METRICS = [metric.metric_name for metric in PROMETHEUS_METRICS]
         # Initialize lists for the data
         time_data = []
@@ -71,25 +96,25 @@ def graph_data_api():
         current_time = datetime.now()
 
         # Get the time filter from query parameters
-        time_filter = request.args.get('filter', default='1 day')
+        time_filter = request.args.get("filter", default="1 day")
 
         # Determine the start time based on the filter
         time_deltas = {
-            '1 minute': 1 * 60,
-            '5 minutes': 5 * 60,
-            '15 minutes': 15 * 60,
-            '30 minutes': 30 * 60,
-            '1 hour': 60 * 60,
-            '3 hours': 3 * 60 * 60,
-            '6 hours': 6 * 60 * 60,
-            '12 hours': 12 * 60 * 60,
-            '1 day': 24 * 60 * 60,
-            '2 days': 2 * 24 * 60 * 60,
-            '3 days': 3 * 24 * 60 * 60,
-            '15 days': 15 * 24 * 60 * 60,
-            '1 week': 7 * 24 * 60 * 60,
-            '1 month': 30 * 24 * 60 * 60,
-            '3 months': 90 * 24 * 60 * 60,
+            "1 minute": 1 * 60,
+            "5 minutes": 5 * 60,
+            "15 minutes": 15 * 60,
+            "30 minutes": 30 * 60,
+            "1 hour": 60 * 60,
+            "3 hours": 3 * 60 * 60,
+            "6 hours": 6 * 60 * 60,
+            "12 hours": 12 * 60 * 60,
+            "1 day": 24 * 60 * 60,
+            "2 days": 2 * 24 * 60 * 60,
+            "3 days": 3 * 24 * 60 * 60,
+            "15 days": 15 * 24 * 60 * 60,
+            "1 week": 7 * 24 * 60 * 60,
+            "1 month": 30 * 24 * 60 * 60,
+            "3 months": 90 * 24 * 60 * 60,
         }
 
         # Get the time range in seconds
@@ -99,20 +124,20 @@ def graph_data_api():
         end_time = int(current_time.timestamp())
         start_time = end_time - time_range_seconds
 
-        step = '30m'
+        step = "30m"
         # Determine the step based on the time range
         if time_range_seconds <= 60:  # 15 minutes
-            step = '2s'
+            step = "2s"
         elif time_range_seconds <= 900:  # 15 minutes
-            step = '10s'
+            step = "10s"
         elif time_range_seconds <= 3600:  # 1 hour
-            step = '30s'
+            step = "30s"
         elif time_range_seconds <= 86400:  # 1 day
-            step = '5m'
+            step = "5m"
         elif time_range_seconds <= 259200:  # 3 days
-            step = '10m'
+            step = "10m"
         elif time_range_seconds <= 604800:  # 1 week
-            step = '30m'
+            step = "30m"
 
         # Cache key generation
         cache_key = f"{time_filter}_{start_time}_{end_time}_{step}"
@@ -121,20 +146,21 @@ def graph_data_api():
             return jsonify(cached_data), 200
 
         # Fetch all metrics asynchronously
-        results = asyncio.run(fetch_all_metrics(PROMETHEUS_METRICS, start_time, end_time, step))
+        results = asyncio.run(
+            fetch_all_metrics(PROMETHEUS_METRICS, start_time, end_time, step)
+        )
 
         # Process the results and populate time_data and metric_data
         for idx, metric in enumerate(PROMETHEUS_METRICS):
-            result = results[idx].get('data', {}).get('result', [])
+            result = results[idx].get("data", {}).get("result", [])
             if result:
                 metric_data[metric] = []
                 for series in result:
-                    series_data = {
-                        "metric": series.get("metric"),
-                        "values": {}
-                    }
+                    series_data = {"metric": series.get("metric"), "values": {}}
                     for value in series.get("values", []):
-                        timestamp = datetime.fromtimestamp(float(value[0]), tz=timezone.utc).isoformat()
+                        timestamp = datetime.fromtimestamp(
+                            float(value[0]), tz=timezone.utc
+                        ).isoformat()
                         if timestamp not in time_data:
                             time_data.append(timestamp)
                         series_data["values"][timestamp] = value[1]
@@ -146,14 +172,21 @@ def graph_data_api():
         # Align data for each metric
         for metric, series_list in metric_data.items():
             for series in series_list:
-                aligned_values = [series["values"].get(timestamp, None) for timestamp in time_data]
+                aligned_values = [
+                    series["values"].get(timestamp, None) for timestamp in time_data
+                ]
                 series["values"] = aligned_values
 
         # Prepare the final response
         response_data = {
             "time": time_data,
-            **{metric: [{"metric": s["metric"], "values": s["values"]} for s in series_list] for metric, series_list in metric_data.items()},
-            "current_time": current_time
+            **{
+                metric: [
+                    {"metric": s["metric"], "values": s["values"]} for s in series_list
+                ]
+                for metric, series_list in metric_data.items()
+            },
+            "current_time": current_time,
         }
 
         # Cache the response
@@ -164,79 +197,122 @@ def graph_data_api():
 
     except Exception as e:
         # Handle and log the error for debugging purposes
-        return jsonify({'error': 'An error occurred while fetching the graph data', 'details': str(e)}), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while fetching the graph data",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
 
-@app.route('/api/v1/targets', methods=['GET'])
+
+@app.route("/api/v1/targets", methods=["GET"])
 @admin_required
 def get_prometheus_targets():
     try:
         # Query Prometheus API to get the targets
         response = requests.get(TARGETS_API_URL)
-        
+
         # Check if the request was successful
         if response.status_code == 200:
-            targets_data = response.json().get('data', {})
-            active_targets = targets_data.get('activeTargets', [])
-            dropped_targets = targets_data.get('droppedTargets', [])
-            
+            targets_data = response.json().get("data", {})
+            active_targets = targets_data.get("activeTargets", [])
+            dropped_targets = targets_data.get("droppedTargets", [])
+
             # Return the active and dropped targets as JSON
-            return jsonify({
-                'active_targets': active_targets,
-                'dropped_targets': dropped_targets
-            }), 200
+            return (
+                jsonify(
+                    {
+                        "active_targets": active_targets,
+                        "dropped_targets": dropped_targets,
+                    }
+                ),
+                200,
+            )
         else:
             # Handle non-200 responses from Prometheus
-            return jsonify({
-                'error': 'Failed to fetch targets from Prometheus',
-                'details': response.text
-            }), response.status_code
-    
+            return (
+                jsonify(
+                    {
+                        "error": "Failed to fetch targets from Prometheus",
+                        "details": response.text,
+                    }
+                ),
+                response.status_code,
+            )
+
     except Exception as e:
         # Handle exceptions
-        return jsonify({
-            'error': 'An error occurred while fetching Prometheus targets',
-            'details': str(e)
-        }), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while fetching Prometheus targets",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
 
 
-@app.route('/api/v1/refresh-interval', methods=['GET', 'POST'])
+@app.route("/api/v1/refresh-interval", methods=["GET", "POST"])
 @login_required
 def manage_refresh_interval():
     try:
-        if request.method == 'GET':
-            settings = UserDashboardSettings.query.filter_by(user_id=current_user.id).first()
+        if request.method == "GET":
+            settings = UserDashboardSettings.query.filter_by(
+                user_id=current_user.id
+            ).first()
             if not settings:
-                return jsonify({'refresh_interval': 30}), 200  # Default to 30 seconds
-            return jsonify({
-                "success": "Refresh interval fetched successfully",
-                'refresh_interval': settings.refresh_interval
-            }), 200
+                return jsonify({"refresh_interval": 30}), 200  # Default to 30 seconds
+            return (
+                jsonify(
+                    {
+                        "success": "Refresh interval fetched successfully",
+                        "refresh_interval": settings.refresh_interval,
+                    }
+                ),
+                200,
+            )
 
         # Update refresh interval (POST request)
-        if request.method == 'POST':
-            new_interval = request.json.get('refresh_interval')
+        if request.method == "POST":
+            new_interval = request.json.get("refresh_interval")
 
             # Validate the new refresh interval
             if not isinstance(new_interval, int) or new_interval <= 0:
-                return jsonify({'error': 'Invalid refresh interval value'}), 400
+                return jsonify({"error": "Invalid refresh interval value"}), 400
 
             # Find or create the user settings
-            settings = UserDashboardSettings.query.filter_by(user_id=current_user.id).first()
+            settings = UserDashboardSettings.query.filter_by(
+                user_id=current_user.id
+            ).first()
             if not settings:
-                settings = UserDashboardSettings(user_id=current_user.id, refresh_interval=new_interval)
+                settings = UserDashboardSettings(
+                    user_id=current_user.id, refresh_interval=new_interval
+                )
                 db.session.add(settings)
             else:
                 settings.refresh_interval = new_interval
 
             settings.save()
-            return jsonify({'success': 'Refresh interval updated successfully', 'refresh_interval': new_interval}), 200
+            return (
+                jsonify(
+                    {
+                        "success": "Refresh interval updated successfully",
+                        "refresh_interval": new_interval,
+                    }
+                ),
+                200,
+            )
 
     except Exception as e:
         db.session.rollback()
-        return jsonify({'error': 'An error occurred', 'details': str(e)}), 500
+        return jsonify({"error": "An error occurred", "details": str(e)}), 500
 
 
-@app.route('/api/v1/os-info', methods=['GET'])
+@app.route("/api/v1/os-info", methods=["GET"])
 @login_required
 def get_os_info_api():
     os_info = get_cached_value("os_info", get_os_info)
@@ -244,7 +320,15 @@ def get_os_info_api():
         os_info.update(get_cached_value("os_release_info", get_os_release_info))
         return jsonify(os_info), 200
     except Exception as e:
-        return jsonify({"error": "An error occurred while fetching the OS information", "details": str(e)}), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while fetching the OS information",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
 
 
 # Endpoint to view the current configuration
@@ -274,14 +358,30 @@ def manage_alert_rules():
     rules = load_alert_rules()
     return jsonify(rules)
 
+
 # Reload Prometheus configuration
 def reload_prometheus():
     url = f"{PROMETHEUS_BASE_URL}/-/reload"
     response = requests.post(url)
     if response.status_code == 200:
-        return jsonify({"status": "success", "message": "Prometheus configuration reloaded"}), 200
+        return (
+            jsonify(
+                {"status": "success", "message": "Prometheus configuration reloaded"}
+            ),
+            200,
+        )
     else:
-        return jsonify({"status": "error", "message": "Failed to reload Prometheus config", "details": response.text}), response.status_code
+        return (
+            jsonify(
+                {
+                    "status": "error",
+                    "message": "Failed to reload Prometheus config",
+                    "details": response.text,
+                }
+            ),
+            response.status_code,
+        )
+
 
 @app.route("/api/v1/prometheus/reload", methods=["POST"])
 def reload_prometheus_config():
@@ -307,42 +407,55 @@ def ready_prometheus():
             response.status_code,
         )
 
+
 # Route to get the current retention time
-@app.route('/api/v1/get-retention', methods=['GET'])
+@app.route("/api/v1/get-retention", methods=["GET"])
 def get_retention():
     try:
         # Fetch current flags from Prometheus
         response = requests.get(f"{PROMETHEUS_BASE_URL}/api/v1/status/flags")
-        flags = response.json().get('data', {})
+        flags = response.json().get("data", {})
 
         # Get the current value of "storage.tsdb.retention.time"
         retention_time = flags.get("storage.tsdb.retention.time", "unknown")
         if retention_time == "0s":
-            retention_time = "Forever"  
+            retention_time = "Forever"
 
-        return jsonify({'retention_time': retention_time}), 200
+        return jsonify({"retention_time": retention_time}), 200
 
     except requests.RequestException as e:
-        return jsonify({'error': 'Failed to fetch retention time from Prometheus', 'details': str(e)}), 500
+        return (
+            jsonify(
+                {
+                    "error": "Failed to fetch retention time from Prometheus",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
     except Exception as e:
-        return jsonify({'error': 'Failed to retrieve retention time', 'details': str(e)}), 500
+        return (
+            jsonify({"error": "Failed to retrieve retention time", "details": str(e)}),
+            500,
+        )
 
 
 # alert history api
-@app.route('/api/v1/alerts/history', methods=['GET'])
+@app.route("/api/v1/alerts/history", methods=["GET"])
 def alert_history_api():
     try:
         alert_data = AlertTicket.query.all()
         if not alert_data:
             return jsonify({"message": "No alert history found."}), 404
-        
+
         return jsonify([alert.serialize() for alert in alert_data]), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/api/v1/chart-configurations')
+
+@app.route("/api/v1/chart-configurations")
 def get_chart_configurations():
-    
+
     user_id = current_user.id
     user = UserProfile.query.get(user_id)
     if not user:
@@ -351,8 +464,9 @@ def get_chart_configurations():
     chart_configurations = ChartConfiguration.query.filter_by(user_id=user_id).all()
     return jsonify([config.serialize() for config in chart_configurations])
 
-@app.route('/api/v1/labels', methods=['GET'])
+
+@app.route("/api/v1/labels", methods=["GET"])
 def labels():
     response = requests.get("http://localhost:9090/api/v1/label/__name__/values")
     data = response.json().get("data", [])
-    return render_template('graphs/labels.html', labels=data)
+    return render_template("graphs/labels.html", labels=data)
