7f2a44beffaabdfe0f4f633d34ef716131066f9c
codeperfectplus <deepak008@live.com>
2025-06-19 12:49:18 +0530

    aws metadata config updated in the config.ini file

--- src/helper/ec2_metadata.py
+++ src/helper/ec2_metadata.py
@@ -1,18 +1,17 @@
 # cython: language_level=3
-import os
 import requests
 
 from functools import lru_cache
 
 from src.helper.logger import get_logger
 from src.config.config_loader import configuration_settings
-
 logger = get_logger(__name__)
-
-cache = {}
 
 CACHE_EXPIRATION = configuration_settings.getint("metrics.settings", "CACHE_EXPIRATION")
 DIVIDE_BY_1024 = configuration_settings.getboolean("metrics.settings", "DIVIDE_BY_1024")
+METADATA_URL = configuration_settings.get("aws.metadata", "METADATA_URL")
+TOKEN_TTL_SECONDS = configuration_settings.getint("aws.metadata", "TOKEN_TTL_SECONDS", fallback=21600)  # Default to 6 hours
+TOKEN_URL = configuration_settings.get("aws.metadata", "TOKEN_URL")
 
 CONVERSION_FACTOR_MB = 1024**2 if DIVIDE_BY_1024 else 1000**2
 CONVERSION_FACTOR_GB = 1024**3 if DIVIDE_BY_1024 else 1000**3
@@ -25,23 +24,22 @@ def get_instance_metadata():
     Returns:
         dict: A dictionary containing all instance metadata.
     """
-    base_url = "http://169.254.169.254/latest/meta-data/"
     metadata = {}
 
     try:
         logger.info("Fetching instance metadata")
         # Get a session token for IMDSv2
         token_response = requests.put(
-            "http://169.254.169.254/latest/api/token",
+            TOKEN_URL,
             headers={
-                "X-aws-ec2-metadata-token-ttl-seconds": "21600"
+                "X-aws-ec2-metadata-token-ttl-seconds": str(TOKEN_TTL_SECONDS)
             },  # Token valid for 6 hours
         )
         token_response.raise_for_status()
         token = token_response.text
 
         # Get all metadata categories
-        response = requests.get(base_url, headers={"X-aws-ec2-metadata-token": token})
+        response = requests.get(METADATA_URL, headers={"X-aws-ec2-metadata-token": token})
         response.raise_for_status()  # Raise an error for bad responses
 
         # List all metadata keys
@@ -49,7 +47,7 @@ def get_instance_metadata():
 
         for key in keys:
             # Fetch each piece of metadata
-            full_url = f"{base_url}{key}"
+            full_url = f"{METADATA_URL}{key}"
             value_response = requests.get(
                 full_url, headers={"X-aws-ec2-metadata-token": token}
             )
